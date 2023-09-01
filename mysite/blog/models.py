7aca37e7918c6e802b74b33b0aff433c4862c91b
Stepan Kolodkin <zorokon@yandex.ru>
2023-09-01 15:04:48 +0300

    delete comment code from blog

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -36,9 +36,6 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-    # def get_absolute_url(self):
-    #     return reverse('blog:post_detail', args=[self.id])
-
     def get_absolute_url(self):
         return reverse('blog:post_detail',
                        args=[
--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -6,6 +6,5 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', post_list, name='post_list'),
-    # path('<int:post_id>/', post_detail, name='post_detail'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='post_detail'),
 ]
