c6e9b9ad82fe43b0bcf91a72fb35af75dcb26df6
Stepan Kolodkin <zorokon@yandex.ru>
2023-09-01 10:23:20 +0300

    delete post_detail first variant

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpRequest, HttpResponse, Http404
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, get_object_or_404
 
 from .models import Post
@@ -7,14 +7,6 @@ from .models import Post
 def post_list(request: HttpRequest) -> HttpResponse:
     posts = Post.published.all()
     return render(request, 'blog/post/list.html', {'posts': posts})
-
-
-def post_detail(request: HttpRequest, post_id: int) -> [HttpResponse, None]:
-    try:
-        post = Post.published.get(id=post_id)
-    except Post.DoesNotExist:
-        raise Http404("No Post found.")
-    return render(request, 'blog/post/detail.html', {'post': post})
 
 
 def post_detail(request: HttpRequest, post_id: int) -> [HttpResponse, None]:
