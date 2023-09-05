b012621d96502b194b1f75a7222a1eebbe99ead3
SKolodkin <dev1@instrument-fit.ru>
2023-09-05 08:29:05 +0300

    create pagination from blog

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,11 +1,23 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Post
 
 
 def post_list(request: HttpRequest) -> HttpResponse:
-    posts = Post.published.all()
+    posts_list = Post.published.all()
+
+    paginator = Paginator(posts_list, 3)
+    page_number = request.GET.get('page', 1)
+
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
